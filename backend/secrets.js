6464fbe24460d28148612c81fa2a4d734e13f288
jogidipen <jogidipen@gmail.com>
2021-03-19 20:26:55 +0530

    Update secrets.js

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURI: 'mongodb+srv://jogidipen:Dipen123$@cluster0.ouxvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  dbURI: 'mongodb+srv://jogidipen:Dipen123%24@cluster0.ouxvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   jwtKey: 'jogidipen'
 };
